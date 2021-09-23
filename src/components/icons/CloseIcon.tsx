c02cee1a795169fc66894047f43ab1f94bb87c37
marvelopers <marvelopers@gmail.com>
2021-09-23 23:16:46 +0900

    아이콘 컴포넌트

---
+++ src/components/icons/CloseIcon.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const CloseIcon = () => (
+  <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M1.25 15.7414L15.75 1.25862" stroke="black" strokeWidth="1.5" strokeLinecap="square" />
+    <path d="M15.75 15.7414L1.25 1.25862" stroke="black" strokeWidth="1.5" strokeLinecap="square" />
+  </svg>
+);
+
+export default CloseIcon;
