db0f9cf7733a80024411d3f16f1ef116f9031558
marvelopers <marvelopers@gmail.com>
2021-09-25 14:37:13 +0900

    정렬 필터 타입 설정

---
+++ src/constants/filter.ts
@@ -0,0 +1,17 @@
+export enum FilterType {
+  Date = 'date',
+  Rating = 'rating',
+  Relevance = 'relevance',
+  Title = 'title',
+  VideoCount = 'videoCount',
+  ViewCount = 'viewCount',
+}
+
+export const FilterList = {
+  [FilterType.Date]: '시간순',
+  [FilterType.Rating]: '평가순',
+  [FilterType.Relevance]: '관려도순',
+  [FilterType.ViewCount]: '조회수순',
+  [FilterType.Title]: '제목순',
+  [FilterType.VideoCount]: '업로드 동영상 갯수순',
+};
